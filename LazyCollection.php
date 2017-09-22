a39b48a2b76ca2de1fdfd7abab91b55fa69619d6
Harmen Janssen <harmen@grrr.nl>
2017-09-22 23:08:57 +0200

    First draft

--- LazyCollection.php
+++ LazyCollection.php
@@ -1,98 +1,213 @@
 <?php
 namespace Lazy;
 
-trait Iterator {
+abstract class LazyCollection {
+
+    abstract public function start();
+
+    abstract public function done($iteration): bool;
+
+    abstract public function value($iteration);
+
+    abstract public function next($iteration);
+
+
+    public function all(): \Generator {
+        $iteration = $this->start();
+        while (!$this->done($iteration)) {
+            yield $this->value($iteration);
+            $iteration = $this->next($iteration);
+        }
+    }
 
     public function first() {
-        return $this->next();
+        return $this->all()->current();
     }
 
     public function take(int $amount): \Generator {
-    /**
-     * @todo Would love it if rewind was unneccessary. But also this brings it back to 0 even though
-     * we started at 10.
-     */
-        $this->rewind();
-        $n = 0;
-        while ($n < $amount || $this->done()) {
-            $n += 1;
-            yield $this->next();
+        $iterations = 0;
+        foreach ($this->all() as $n) {
+            if ($iterations >= $amount) {
+                break;
+            }
+            yield $n;
+            $iterations++;
         }
+    }
+
+    public function map($transformer): Mapped {
+        return new Mapped($this, $transformer);
     }
 
 }
 
-class FilteredIterator {
-    use Iterator;
+class Mapped extends LazyCollection {
 
-    protected $_collection;
-    protected $_predicate;
+    protected $collection;
+
+    protected $transformer;
+
+    public function __construct(LazyCollection $collection, callable $transformer) {
+        $this->collection = $collection;
+        $this->transformer = $transformer;
+    }
+
+    public function start() {
+        return $this->collection->start();
+    }
+
+    public function done($iteration): bool {
+        return $this->collection->done($iteration);
+    }
+
+    public function value($iteration) {
+        return ($this->transformer)($this->collection->value($iteration));
+    }
+
+    public function next($iteration) {
+        return $this->collection->next($iteration);
+    }
+
+}
+
+class Filtered extends LazyCollection {
+
+    protected $collection;
+
+    protected $predicate;
 
     public function __construct(LazyCollection $collection, callable $predicate) {
-        $this->_collection = $collection;
-        $this->_predicate = $predicate;
+        $this->collection = $collection;
+        $this->predicate = $predicate;
     }
 
-    public function next() {
-        while (!$this->_collection->done()) {
-            $next = $this->_collection->next();
-            if ($this->_predicate($next)) {
-                yield $next;
+    public function start() {
+        return $this->collection->start();
+    }
+
+    public function done($iteration): bool {
+        return $this->collection->done($iteration);
+    }
+
+    public function value($iteration) {
+        return $this->collection->value($iteration);
+    }
+
+    public function next($iteration) {
+        return $this->collection->next($iteration);
+    }
+
+}
+
+class xList extends LazyCollection {
+
+    protected $_data;
+
+    public function __construct(array $data) {
+        $this->_data = $data;
+    }
+
+    public function start() {
+        return 0;
+    }
+
+    public function done($iteration): bool {
+        return $iteration >= count($this->_data);
+    }
+
+    public function value($iteration) {
+        return $this->_data[$iteration];
+    }
+
+    public function next($iteration) {
+        return $iteration + 1;
+    }
+
+    public function filter($predicate) {
+        return [
+            'iterator' => function() use ($predicate) {
+                foreach ($this->all() as $i) {
+                    if ($predicate($i)) {
+                        yield $i;
+                    }
+                }
             }
-        }
+        ];
     }
 
 }
 
-abstract class LazyCollection {
-    use Iterator;
-
-    abstract public function done();
-    abstract public function next();
-    abstract public function rewind();
-
-    public function filter(callable $predicate) {
-        return new FilteredIterator($this->rewind(), $predicate);
-    }
-
-}
-
+/*
 class Numbers extends LazyCollection {
 
-    protected $_index = 0;
+    protected $_start = 0;
 
-    /**
-     * @todo Would love it if rewind was unneccessary.....
-     */
-    public function rewind() {
-        $this->_index = 0;
+    public function init() {
+        return $this->_start;
     }
 
-    public function done() {
+    public function done($value) {
         // infinite collection
         return false;
     }
 
-    public function next() {
-        $result = $this->_index;
-        $this->_index += 1;
-        return $result;
+    public function next($prev) {
+        return $prev + 1;
     }
 
-    private function __construct(int $from) {
-        $this->_index = $from;
+    private function __construct(int $start) {
+        $this->_start = $start;
     }
 
-    static public function from(int $from) {
-        return new Numbers($from);
+    static public function from(int $start) {
+        return new Numbers($start);
     }
 
 }
+ */
 
 function out($in) {
     echo "{$in}\n";
 }
 
+$square = function ($n) {
+    return $n * $n;
+};
+
+$isEven = function (int $n): bool {
+    return $n % 2 === 0;
+};
+
+$list = new xList([0, 1, 2, 3, 4, 5]);
+foreach ($list->all() as $n) {
+    out($n);
+}
+
+out('squares:');
+foreach ($list->map($square)->all() as $n) {
+    out($n);
+}
+
+out('even numbers:');
+foreach ($list->filter($isEven)['iterator']() as $n) {
+    out($n);
+}
+
+out('FIRST!');
+out($list->first());
+
+out('Take 5');
+foreach ($list->take(5) as $n) {
+    out($n);
+}
+
+out('Take 5');
+foreach ($list->take(5) as $n) {
+    out($n);
+}
+
+
+/*
 out("Working with numbers from 10");
 $numbers = Numbers::from(10);
 
@@ -111,3 +226,12 @@ $isEven = function (int $n): bool {
 foreach ($numbers->filter($isEven)->take(5) as $n) {
     out($n);
 }
+
+out('Squares numbers');
+$square = function ($n) {
+    return $n * $n;
+};
+foreach ($numbers->map($square)->take(5) as $n) {
+    out($n);
+}
+ */
